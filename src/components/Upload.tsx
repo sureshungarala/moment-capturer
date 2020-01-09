de1244d798cf45713f05b2d8e3c26c3413057484
Ungarala Suresh <iamuvvsuresh@gmail.com>
2020-01-09 18:18:20 +0530

    to single image

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -38,20 +38,39 @@ export default class extends React.Component<uploadProps, uploadState>{
         event.preventDefault();
         console.log('Files submitted.');
         if (this.state.files && this.state.files.length !== 0) {
-            let formdata = new window.FormData();
-            formdata.append('image', this.state.files[0], this.state.files[0].name);
-            try {
-                const response = fetch('https://api.momentcapturer.com/csr', {
-                    method: 'POST',
-                    headers: {
-                        'content-type': 'multipart/form-data'
-                    },
-                    body: formdata
+            const start = window.performance.now();
+            let image = new window.Image(),
+                base64 = '',
+                imageName = this.state.files[0].name
+            image.onload = () => {
+                let canvas = document.createElement('canvas'),
+                    context = canvas.getContext('2d');
+                context && context.drawImage(image, 0, 0, image.width, image.height);
+                base64 = canvas.toDataURL('image/jpeg', 1.0);
+                console.log(window.performance.now() - start);
+                console.log(base64);
+                let body = JSON.stringify({
+                    image: base64,
+                    imageName: imageName,
+                    resolution: image.width + ':' + image.height
                 });
-                console.log('CSR succeeded with response ', response);
-            } catch (err) {
-                console.log('CSR failed with error ', err);
-            }
+
+                try {
+                    const response = fetch('https://api.momentcapturer.com/csr', {
+                        method: 'POST',
+                        mode: 'cors',
+                        headers: {
+                            'content-type': 'application/json',
+                            'accept': 'application/json'
+                        },
+                        body: body
+                    });
+                    console.log('CSR succeeded with response ', response);
+                } catch (err) {
+                    console.log('CSR failed with error ', err);
+                }
+            };
+            image.src = window.URL.createObjectURL(this.state.files[0]);
         }
     }
 
@@ -61,7 +80,7 @@ export default class extends React.Component<uploadProps, uploadState>{
             <form onSubmit={this.handleSubmit} className="upload-form">
                 <input type="button" value="Choose files to Upload" onClick={this.openFileDialog} />
                 <span>{`${fileCount} file${fileCount === 1 ? 's' : ''}`} selected</span>
-                <input type="file" accept="image/*" ref={this.fileRef} onChange={this.handleChange} multiple style={{ visibility: 'hidden' }} />
+                <input type="file" accept="image/*" ref={this.fileRef} onChange={this.handleChange} style={{ visibility: 'hidden' }} />
                 <input type="submit" value="Upload" />
             </form>
         );
