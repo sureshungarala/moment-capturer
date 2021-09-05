f7537459560c4d826713b9bfa1f336e1af2f30f3
sureshUngarala <iamuvvsuresh@gmail.com>
2021-09-05 21:08:19 +0530

    deferring images till on viewport & styling on focus

--- src/components/Utils/CategoryCard.tsx
+++ src/components/Utils/CategoryCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 interface CardProps {
@@ -8,20 +8,45 @@ interface CardProps {
   categoryTag: string;
 }
 
-const CategoryCard: React.FunctionComponent<CardProps> = (props: CardProps) => (
-  <div className="categoryCard">
-    <NavLink to={`/${props.categoryTag}`}>
-      <div className="thumbnail">
-        {props.src && <img src={props.src} alt={props.altText} />}
-        {!props.src && (
-          <div className="fallback">
-            <span>No moments yet. Check again after some time.</span>
-          </div>
-        )}
-      </div>
-      <div className="title">{props.title}</div>
-    </NavLink>
-  </div>
-);
+const CategoryCard: React.FunctionComponent<CardProps> = (props: CardProps) => {
+  const [cardTemplate, setCardTemplate] = useState<React.ReactFragment>(
+    <div className="fallback"></div>
+  );
+  const cardRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (props.src) {
+      const observer = new window.IntersectionObserver(
+        (entries: Array<IntersectionObserverEntry>) => {
+          const [{ isIntersecting }] = entries;
+          if (isIntersecting) {
+            setCardTemplate(<img alt={props.altText} src={props.src} />);
+            observer.disconnect();
+          }
+        },
+        {
+          root: null,
+          rootMargin: "0px 0px 200px 0px",
+        }
+      );
+      observer.observe(cardRef.current as Element);
+    } else {
+      setCardTemplate(
+        <div className="fallback">
+          <span>No moments yet. Check again after some time.</span>
+        </div>
+      );
+    }
+  }, []);
+
+  return (
+    <div className="categoryCard" ref={cardRef}>
+      <NavLink to={`/${props.categoryTag}`}>
+        <div className="thumbnail">{cardTemplate}</div>
+        <div className="title">{props.title}</div>
+      </NavLink>
+    </div>
+  );
+};
 
 export default CategoryCard;
