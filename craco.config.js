8899e9e9c44dc082edb773c0f6b0325bac3fc101
sureshUngarala <iamuvvsuresh@gmail.com>
2021-10-31 14:30:13 +0530

    Update react-scripts to v4.0.3 Added craco for preact & resolving urls in CRA v4.x.x updated all url(...path) usage relative to public folder

---
+++ craco.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  style: {
+    css: {
+      loaderOptions: () => {
+        return { url: false };
+      },
+    },
+    sass: {
+      loaderOptions: () => ({
+        // Prefer `dart-sass`
+        implementation: require("sass"),
+      }),
+    },
+  },
+  webpack: {
+    alias: {
+      react: "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat", // Must be below test-utils
+      "react/jsx-runtime": "preact/jsx-runtime",
+    },
+  },
+};
