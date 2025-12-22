730ba6f8c47fac2078a96c8f8e41adf8fb5838c3
VijayaVenkataSuresh Ungarala <vijayavenkatasuresh.ungarala@arcticwolf.com>
2025-12-22 15:10:48 +0530

    feat: implement masonry layout

--- src/components/CategoryHome.tsx
+++ src/components/CategoryHome.tsx
@@ -1,17 +1,14 @@
-import React, { useCallback, useEffect, useReducer } from 'react';
+import React, { useEffect } from 'react';
+import Masonry from 'react-masonry-css';
 import { useParams } from 'react-router-dom';
-import { Auth } from '@aws-amplify/auth';
 
 import Loader from './Utils/Loader';
 import Banner from './Utils/Banner';
 import ImageComponent from './Utils/Image';
 import '../styles/templates/categoryHome.scss';
 
-import {
-  CategoryHomeReducer,
-  CategoryHomeState,
-  CategoryHomeActions,
-} from '../Reducers/CategoryHomeReducer';
+import { useQuery } from '@tanstack/react-query';
+import PageTransition from './Utils/PageTransition';
 
 import { fetchImages, checkIfUserSignedIn } from '../utils/apis';
 import { Image, McMoments } from '../info/types';
@@ -24,61 +21,50 @@ import { initMoments } from '../utils/constants';
 
 interface homeProps {}
 
+const breakpointColumnsObj = {
+  default: 3,
+  1100: 2,
+  700: 1,
+};
+
+interface homeProps {}
+
 const Home: React.FunctionComponent<homeProps> = () => {
   const params = useParams();
-  const [state, dispatch] = useReducer(CategoryHomeReducer, {
-    images: initMoments,
-    fetchingImages: true,
-    fetchingFailed: false,
-    userSignedIn: false,
-  });
   const { tag } = getMappedCategory(params.categoryTag!);
 
-  /* --------------------- data fetch start---------------------- */
-  const fetchCategoryImages = useCallback((tag: string) => {
-    dispatch({
-      type: CategoryHomeActions.FETCHING_IMAGES,
-    });
+  const {
+    data: imagesData,
+    isLoading: fetchingImages,
+    isError: fetchingFailed,
+    refetch,
+  } = useQuery({
+    queryKey: ['images', tag],
+    queryFn: () => fetchImages(tag),
+    select: (data) => reOrderImages(data.images),
+  });
 
-    Promise.all([fetchImages(tag), checkIfUserSignedIn(Auth)]).then(
-      ([data, userSignedIn]) => {
-        const images: McMoments = reOrderImages(data.images);
-        dispatch({
-          type: CategoryHomeActions.FETCH_SUCCESS,
-          images,
-          userSignedIn,
-        });
-        if (userSignedIn) dispachSignedInEvent();
-      },
-      (_error) => {
-        dispatch({
-          type: CategoryHomeActions.FETCH_FAILED,
-        });
-      }
-    );
-  }, []);
+  const { data: userSignedIn } = useQuery({
+    queryKey: ['userSignedIn'],
+    queryFn: checkIfUserSignedIn,
+    initialData: false,
+  });
 
   useEffect(() => {
-    fetchCategoryImages(tag);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [tag]);
-  /* --------------------- data fetch end---------------------- */
-
-  const {
-    images: { biotc, moments: images },
-    fetchingImages,
-    fetchingFailed,
-    userSignedIn,
-  }: CategoryHomeState = state;
+    if (userSignedIn) {
+      dispachSignedInEvent();
+    }
+  }, [userSignedIn]);
 
   let categoryHome = <></>;
   if (fetchingImages) {
     categoryHome = <Loader />;
   } else if (fetchingFailed) {
-    categoryHome = (
-      <Banner canRefresh onRefresh={() => fetchCategoryImages(tag)} />
-    );
-  } else if (!(images.length || biotc.original.length)) {
+    categoryHome = <Banner canRefresh onRefresh={() => refetch()} />;
+  } else if (
+    !imagesData ||
+    !(imagesData.moments.length || imagesData.biotc.original.length)
+  ) {
     categoryHome = (
       <Banner
         title={
@@ -87,31 +73,40 @@ const Home: React.FunctionComponent<homeProps> = () => {
       />
     );
   } else {
+    const { biotc, moments: images } = imagesData;
     categoryHome = (
       <React.Fragment>
         {biotc.original.length > 0 && (
           <ImageComponent
             {...biotc}
-            userSignedIn={userSignedIn}
+            userSignedIn={!!userSignedIn}
             categoryTag={tag}
           />
         )}
-        <div className='images-container'>
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className='my-masonry-grid'
+          columnClassName='my-masonry-grid_column'
+        >
           {images.map((image: Image) => {
             return (
               <ImageComponent
                 {...image}
-                userSignedIn={userSignedIn}
+                userSignedIn={!!userSignedIn}
                 categoryTag={tag}
                 key={image.updateTime}
               />
             );
           })}
-        </div>
+        </Masonry>
       </React.Fragment>
     );
   }
-  return <div className='category-home'>{categoryHome}</div>;
+  return (
+    <PageTransition>
+      <div className='category-home'>{categoryHome}</div>
+    </PageTransition>
+  );
 };
 
 export default Home;
